57663e840e2df7efdbf9465eda0d9b4e02f87351
Ektasinghhh13 <ektasingh62849@gmail.com>
2022-10-29 23:50:11 +0530

    Ref and populate

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
 
+const newAuthorController =require('../controllers/newAuthorController')
+const newBookController=require('../controllers/newBookVController')
+const newPublisherController=require('../controllers/newPublisherController')
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -17,5 +21,14 @@ router.post("/createBook", bookController.createBook  )
 router.get("/getBooksData", bookController.getBooksData)
 
 router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+
+router.post('/newCreateAuthor',newAuthorController.newCreateAuthor)
+
+router.post('/newCreateBook',newBookController.newCreateBook)
+router.post('/newPublisher',newPublisherController.newPublisher)
+
+router.get('/fetchbookdata',newBookController.fetchbookdata)
+
+router.put('/update',newBookController.update)
 
 module.exports = router;
